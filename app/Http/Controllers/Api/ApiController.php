bb68d304f7258a07b9293197b87dec9b0681a341
MXbal <imshroud584@gmail.com>
2022-03-20 09:56:07 +0700

    Upload Project

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -287,7 +287,7 @@ class ApiController extends Controller
                         //     echo json_encode($response);
                         // }
 
-                        if ($now >= $startMasuk && $now <= $endKeluar) {
+                        if ($now >= $startMasuk && $now < $endKeluar) {
                             $absen = true;
                             $ket = "Hadir";
                             $status = 1;
